a703cccfa2accdd92a0a3c2365fae4a8ebf7c56d
Vinicius Bueno Costa <buenov535@gmail.com>
2024-02-28 17:55:49 -0300

    test: adicionadas funções auxiliares

--- src/tests/utils.tsx
+++ src/tests/utils.tsx
@@ -1,6 +1,39 @@
 import { render } from '@testing-library/react';
 import { ReactElement } from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
+const wrapWithRouter = (ui: ReactElement, initialEntries = ['/']) => {
+  return (
+    <MemoryRouter initialEntries={ initialEntries }>
+      {ui}
+    </MemoryRouter>
+  );
+};
+
+const wrapWithProviders = (ui: ReactElement) => {
+  const combinedProviders = (children: ReactElement) => (<p>{children}</p>);
+  return combinedProviders(ui);
+};
 
 export const renderWithContext = (ui: ReactElement) => {
-  return render(ui);
+  // está faltando o provider do contexto
+  return render(wrapWithProviders(ui));
+};
+
+export const renderWithRouter = (ui: ReactElement, { initialEntries = ['/'] } = {}) => {
+  return {
+    user: userEvent.setup(),
+    ...render(wrapWithRouter(ui, initialEntries)),
+  };
+};
+
+export const renderWithRouterAndProviders = (
+  ui: ReactElement,
+  { initialEntries = ['/'] } = {},
+) => {
+  return {
+    user: userEvent.setup(),
+    ...render(wrapWithProviders(wrapWithRouter(ui, initialEntries))),
+  };
 };

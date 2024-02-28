764a42c880fdf2c18df20e1450c4fc3ab61a4527
Alisson Breno <alissonbreno36@hotmail.com>
2024-02-28 16:48:23 -0300

    Feat: Adicionando condições aos inputs para habilitar o botão(Requisito 4)

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,43 @@
+import { useState } from 'react';
+
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event: any) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event:any) => {
+    setPassword(event.target.value);
+  };
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const validEmail = emailRegex.test(email);
+
+  const validPassword = password.length > 6;
+
+  const formIsValid = (validEmail && validPassword);
+
   return (
     <div>
-      <input type="email" data-testid="email-input" />
-      <input type="password" data-testid="password-input" />
-      <button data-testid="login-submit-btn">Enter</button>
+      <input
+        type="email"
+        data-testid="email-input"
+        onChange={ handleEmailChange }
+        value={ email }
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        value={ password }
+        onChange={ handlePasswordChange }
+      />
+      <button
+        data-testid="login-submit-btn"
+        disabled={ !formIsValid }
+      >
+        Enter
+      </button>
     </div>
   );
 }

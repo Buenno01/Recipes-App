875dccb096cce3cf58fe2beaeefb58543ad21a4e
Paulo Afonso Cardoso Franco - Turma 36 <paulo_afonso2006@hotmail.com>
2024-02-28 22:45:25 -0300

    fix: limite de apresentação de 2 tags por receita

--- src/components/DoneRecipe.tsx
+++ src/components/DoneRecipe.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { DoneRecipeProps } from '../@types/DoneRecipeType';
 
-
 function DoneRecipe(props: DoneRecipeProps) {
   const { doneRecipe, index } = props;
   return (
     <div>
-      <img src={ doneRecipe.image } data-testid={ `${index}-horizontal-image` } alt={ doneRecipe.name } />
+      <img
+        src={ doneRecipe.image }
+        data-testid={ `${index}-horizontal-image` }
+        alt={ doneRecipe.name }
+      />
       <p data-testid={ `${index}-horizontal-top-text` }>{doneRecipe.category}</p>
       <p data-testid={ `${index}-horizontal-name` }>{doneRecipe.category}</p>
       <p data-testid={ `${index}-horizontal-done-date` }>{doneRecipe.doneDate}</p>
@@ -18,13 +21,15 @@ function DoneRecipe(props: DoneRecipeProps) {
       </button>
       <p>Tags:</p>
       {
-      doneRecipe.tags.map((tagName: string)=>{
-        return(
-        <p key={tagName} data-testid={`${index}-${tagName}-horizontal-tag`}>
-          {tagName}
-        </p>
+      doneRecipe.tags?.map((tagName: string, tagIndex: number) => {
+        if (tagIndex >= 2) return;
+        return (
+          <p key={ tagName } data-testid={ `${index}-${tagName}-horizontal-tag` }>
+            {tagName}
+          </p>
         );
-      })}
+      })
+}
     </div>
   );
 }

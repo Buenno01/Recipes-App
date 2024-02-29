98f4e6fba0160de8afd054fc61a746f99cc60410
Paulo Afonso Cardoso Franco - Turma 36 <paulo_afonso2006@hotmail.com>
2024-02-29 00:13:03 -0300

    feat: apresentação diferente para comida e bebida | feat: criação de imagem para o botão | fix: alteração de elemento responsável por armazenar ID do botão - teste pedia o atributo src no elemento e não aceitou como filho a tag img como filho.

--- src/components/DoneRecipe.tsx
+++ src/components/DoneRecipe.tsx
@@ -10,14 +10,19 @@ function DoneRecipe(props: DoneRecipeProps) {
         alt={ doneRecipe.name }
       />
       <p data-testid={ `${index}-horizontal-top-text` }>
-        {`${doneRecipe.nationality} - ${doneRecipe.category}`}
+        {doneRecipe.type === 'meal'
+          ? `${doneRecipe.nationality} - ${doneRecipe.category}`
+          : `${doneRecipe.alcoholicOrNot}`}
       </p>
       <p data-testid={ `${index}-horizontal-name` }>{doneRecipe.name}</p>
       <p data-testid={ `${index}-horizontal-done-date` }>{doneRecipe.doneDate}</p>
-      <button
-        id="share-done-recipe-button"
-        data-testid={ `${index}-horizontal-share-btn` }
-      >
+      <button>
+        <img
+          data-testid={ `${index}-horizontal-share-btn` }
+          id="share-done-recipe-button"
+          src="src/images/shareIcon.svg"
+          alt="Compartilhar"
+        />
         Compartilhar
       </button>
       <p>Tags:</p>

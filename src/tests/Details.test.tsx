30ffad3255885d3e061a9459b3349c21c44eaefd
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-01 11:13:18 -0300

    test: adicionados para a renderizacao da pagina de detalhes

--- src/tests/Details.test.tsx
+++ src/tests/Details.test.tsx
@@ -1,26 +1,79 @@
-import { screen, waitForElementToBeRemoved } from '@testing-library/dom';
+import { screen } from '@testing-library/dom';
 import { vi } from 'vitest';
 import { renderWithRouter } from './utils';
-import { mealMock } from './mock';
+import { formattedMealMock, mealFormattedSearchByNameMock } from './mock';
 import Details from '../pages/RecipeDetails';
+import * as fetchById from '../services/useFetchDrinkOrFoodById';
+import * as fetchByName from '../services/useFetchDrinkOrFoodByName';
+import { AnyRecipeType } from '../@types/AnyRecipeType';
 
-describe.skip('RecipeDetails', () => {
+type ReturnUseFetchDrinkOrFoodByIdType = {
+  recipe: AnyRecipeType | undefined;
+  loading: boolean;
+  error: string;
+};
+
+type UseFetchDrinkOrFoodByNameReturnType = {
+  recipes: AnyRecipeType[];
+  loading: boolean;
+  error: string;
+};
+
+type FetchReturnsType = ReturnUseFetchDrinkOrFoodByIdType | UseFetchDrinkOrFoodByNameReturnType;
+
+const MOCK_FETCH_BY_ID_RETURN = {
+  recipe: formattedMealMock,
+  loading: false,
+  error: '',
+};
+
+const MOCK_FETCH_BY_NAME_RETURN = {
+  recipes: mealFormattedSearchByNameMock,
+  loading: false,
+  error: '',
+};
+
+const INITIAL_ENTRIES = { initialEntries: ['/meals/52771'] };
+
+const mockFetchs = (module: any, response: FetchReturnsType, method = 'default') => vi.spyOn(module, method).mockReturnValue(response);
+
+describe('RecipeDetails', () => {
   beforeEach(() => {
     vi.restoreAllMocks();
   });
+
   it('should render the details page', async () => {
-    const mockSpy = vi.spyOn(global, 'fetch').mockResolvedValue({
-      json: async () => mealMock,
-      ok: true,
-      status: 200,
-    } as Response);
-    renderWithRouter(<Details />, { initialEntries: ['/meals/52771'] });
-
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
-
-    await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
-
-    expect(mockSpy).toHaveBeenCalledTimes(1);
+    const mockMainFetchSpy = mockFetchs(fetchById, MOCK_FETCH_BY_ID_RETURN);
+    const mockFetchByNameSpy = mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN);
+    renderWithRouter(<Details />, INITIAL_ENTRIES);
     expect(window.location.pathname).toBe('/meals/52771');
+
+    expect(mockMainFetchSpy).toHaveBeenCalledTimes(1);
+    expect(mockFetchByNameSpy).toHaveBeenCalledTimes(1);
+    expect(mockFetchByNameSpy).toHaveBeenCalledWith('', 'drinks');
+  });
+
+  it('should render the meal correctly', async () => {
+    mockFetchs(fetchById, MOCK_FETCH_BY_ID_RETURN);
+    mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN);
+    renderWithRouter(<Details />, INITIAL_ENTRIES);
+
+    const everything = [
+      await screen.findByTestId('recipe-photo'),
+      await screen.findByTestId('recipe-title'),
+      await screen.findByTestId('recipe-category'),
+      await screen.findByTestId('instructions'),
+      await screen.findByTestId('video'),
+    ];
+
+    const [image, title, category, instructions, video] = everything;
+
+    everything.forEach((element) => expect(element).toBeInTheDocument());
+
+    expect(image).toHaveAttribute('src', formattedMealMock.thumb);
+    expect(title).toHaveTextContent(formattedMealMock.name);
+    expect(category).toHaveTextContent(formattedMealMock.category);
+    expect(instructions).toHaveTextContent(formattedMealMock.instructions);
+    expect(video).toHaveAttribute('src', formattedMealMock.video);
   });
 });
--- src/tests/mock.ts
+++ src/tests/mock.ts
@@ -1,4 +1,7 @@
 import { DoneRecipeType } from '../@types/DoneRecipeType';
+import { MealRecipeType } from '../@types/MealRecipeType';
+
+const MEAL_THUMB = 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg';
 
 export const mealMock = {
   meals: [{
@@ -7,7 +10,7 @@ export const mealMock = {
     strCategory: 'Vegetarian',
     strArea: 'Italian',
     strInstructions: 'Cook the pasta in a pan',
-    strMealThumb: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    strMealThumb: MEAL_THUMB,
     strYoutube: 'https://www.youtube.com/watch?v=1IszT_guI08',
     strIngredient1: 'Penne Rigate',
     strIngredient2: 'Olive Oil',
@@ -50,6 +53,92 @@ export const mealMock = {
     strMeasure19: '',
     strMeasure20: '',
   }] };
+
+export const formattedMealMock: MealRecipeType = {
+  type: 'meals',
+  id: '52771',
+  category: 'Vegetarian',
+  name: 'Spicy Arrabiata Penne',
+  thumb: MEAL_THUMB,
+  area: 'Italian',
+  instructions: 'Cook the pasta in a pan',
+  video: 'https://www.youtube.com/watch?v=1IszT_guI08',
+  tags: null,
+  ingredients: [
+    'Penne Rigate',
+    'Olive Oil',
+    'Garlic',
+    'Red Chilli',
+    'Italian Tomatoes',
+    'Dried Oregano',
+    'Red Wine',
+    'Fresh Basil',
+    'Salt',
+    'Black Pepper',
+    'Parmesan',
+    'Water',
+  ],
+  measures: [
+    '1 pound',
+    '1/4 cup',
+    '1 clove',
+    '1',
+    '1 tin',
+    '1 teaspoon',
+    '1/2 cup',
+    '6 leaves',
+    'to taste',
+    'to taste',
+    'to taste',
+    '1/2 cup',
+  ],
+  dateModified: null,
+  creativeCommonsConfirmed: null,
+  imageSource: null,
+  drinkAlternate: null,
+  source: '',
+};
+
+export const mealFormattedSearchByNameMock: MealRecipeType[] = [
+  {
+    ...formattedMealMock,
+  },
+  {
+    ...formattedMealMock,
+    id: '52772',
+    name: 'Teriyaki Chicken',
+  },
+  {
+    ...formattedMealMock,
+    id: '52773',
+    name: 'Salad',
+  },
+  {
+    ...formattedMealMock,
+    id: '52774',
+    name: 'Pasta',
+  },
+  {
+    ...formattedMealMock,
+    id: '52775',
+    name: 'Pizza',
+  },
+  {
+    ...formattedMealMock,
+    id: '52776',
+    name: 'Sushi',
+  },
+  {
+    ...formattedMealMock,
+    id: '52777',
+    name: 'Burger',
+  },
+  {
+    ...formattedMealMock,
+    id: '52778',
+    name: 'Fries',
+  },
+];
 
 export const mealSearchedByIDMock = { meals:
     [
@@ -132,7 +221,7 @@ export const doneRecipesMock: DoneRecipeType[] = [{
   category: 'Chicken',
   alcoholicOrNot: '',
   name: 'Teriyaki Chicken Casserole',
-  image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  image: MEAL_THUMB,
   doneDate: '2017-12-19 18:34:15',
   tags: ['Soup', 'Chicken', 'Tag Que Não Deve Ser Mostrada'],
 },

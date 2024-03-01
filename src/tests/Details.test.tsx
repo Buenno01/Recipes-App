d8483455f2f66a527032670b27abde05f84f8467
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-01 11:57:45 -0300

    test: adicionados testes para as recomendações e falhas de requisição da api na pagina de detalhes

--- src/tests/Details.test.tsx
+++ src/tests/Details.test.tsx
@@ -1,11 +1,12 @@
 import { screen } from '@testing-library/dom';
 import { vi } from 'vitest';
 import { renderWithRouter } from './utils';
-import { formattedMealMock, mealFormattedSearchByNameMock } from './mock';
+import { formattedMealMock, mealFormattedSearchByNameMock } from './mealMocks';
 import Details from '../pages/RecipeDetails';
 import * as fetchById from '../services/useFetchDrinkOrFoodById';
 import * as fetchByName from '../services/useFetchDrinkOrFoodByName';
 import { AnyRecipeType } from '../@types/AnyRecipeType';
+import { formattedDrinkMock, formattedDrinkSearchByNameMock } from './drinkMocks';
 
 type ReturnUseFetchDrinkOrFoodByIdType = {
   recipe: AnyRecipeType | undefined;
@@ -21,14 +22,31 @@ type UseFetchDrinkOrFoodByNameReturnType = {
 
 type FetchReturnsType = ReturnUseFetchDrinkOrFoodByIdType | UseFetchDrinkOrFoodByNameReturnType;
 
-const MOCK_FETCH_BY_ID_RETURN = {
+type RecommendationsReturnType = {
+  cards: HTMLElement[];
+  titles: HTMLElement[];
+};
+
+const MOCK_FETCH_BY_ID_RETURN_MEAL = {
   recipe: formattedMealMock,
   loading: false,
   error: '',
 };
 
-const MOCK_FETCH_BY_NAME_RETURN = {
+const MOCK_FETCH_BY_NAME_RETURN_MEALS = {
   recipes: mealFormattedSearchByNameMock,
+  loading: false,
+  error: '',
+};
+
+const MOCK_FETCH_BY_ID_RETURN_DRINK = {
+  recipe: formattedDrinkMock,
+  loading: false,
+  error: '',
+};
+
+const MOCK_FETCH_BY_NAME_RETURN_DRINKS = {
+  recipes: formattedDrinkSearchByNameMock,
   loading: false,
   error: '',
 };
@@ -37,34 +55,40 @@ const INITIAL_ENTRIES = { initialEntries: ['/meals/52771'] };
 
 const mockFetchs = (module: any, response: FetchReturnsType, method = 'default') => vi.spyOn(module, method).mockReturnValue(response);
 
+const getTheDetailedRecipeInfos = async () => [
+  await screen.findByTestId('recipe-photo'),
+  await screen.findByTestId('recipe-title'),
+  await screen.findByTestId('recipe-category'),
+  await screen.findByTestId('instructions'),
+  await screen.findByTestId('video'),
+];
+
+const getRecomendations = async (): Promise<RecommendationsReturnType> => ({
+  cards: await screen.findAllByTestId(/recommendation-card/i),
+  titles: await screen.findAllByTestId(/recommendation-title/i),
+});
+
 describe('RecipeDetails', () => {
   beforeEach(() => {
     vi.restoreAllMocks();
   });
 
   it('should render the details page', async () => {
-    const mockMainFetchSpy = mockFetchs(fetchById, MOCK_FETCH_BY_ID_RETURN);
-    const mockFetchByNameSpy = mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN);
+    const mockMainFetchSpy = mockFetchs(fetchById, MOCK_FETCH_BY_ID_RETURN_MEAL);
+    const mockFetchByNameSpy = mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN_DRINKS);
     renderWithRouter(<Details />, INITIAL_ENTRIES);
     expect(window.location.pathname).toBe('/meals/52771');
 
     expect(mockMainFetchSpy).toHaveBeenCalledTimes(1);
     expect(mockFetchByNameSpy).toHaveBeenCalledTimes(1);
-    expect(mockFetchByNameSpy).toHaveBeenCalledWith('', 'drinks');
   });
 
   it('should render the meal correctly', async () => {
-    mockFetchs(fetchById, MOCK_FETCH_BY_ID_RETURN);
-    mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN);
+    mockFetchs(fetchById, MOCK_FETCH_BY_ID_RETURN_MEAL);
+    mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN_DRINKS);
     renderWithRouter(<Details />, INITIAL_ENTRIES);
 
-    const everything = [
-      await screen.findByTestId('recipe-photo'),
-      await screen.findByTestId('recipe-title'),
-      await screen.findByTestId('recipe-category'),
-      await screen.findByTestId('instructions'),
-      await screen.findByTestId('video'),
-    ];
+    const everything = await getTheDetailedRecipeInfos();
 
     const [image, title, category, instructions, video] = everything;
 
@@ -75,5 +99,71 @@ describe('RecipeDetails', () => {
     expect(category).toHaveTextContent(formattedMealMock.category);
     expect(instructions).toHaveTextContent(formattedMealMock.instructions);
     expect(video).toHaveAttribute('src', formattedMealMock.video);
+  });
+
+  describe('should fetch the recommendations corretly', () => {
+    it('should fetch the drinks recommendations for meals', async () => {
+      const mockMainFetchSpy = mockFetchs(fetchById, MOCK_FETCH_BY_ID_RETURN_MEAL);
+      const mockFetchByNameSpy = mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN_DRINKS);
+
+      renderWithRouter(<Details />, INITIAL_ENTRIES);
+
+      expect(mockMainFetchSpy).toHaveBeenCalledTimes(1);
+      expect(mockMainFetchSpy).toHaveBeenCalledWith('', 'meals');
+      expect(mockFetchByNameSpy).toHaveBeenCalledTimes(1);
+      expect(mockFetchByNameSpy).toHaveBeenCalledWith('', 'drinks');
+
+      const { cards, titles } = await getRecomendations();
+
+      expect(cards).toHaveLength(6);
+
+      titles.forEach((title, index) => {
+        expect(title).toHaveTextContent(formattedDrinkSearchByNameMock[index].name);
+      });
+    });
+
+    it('should fetch the meals recommendations for drinks', async () => {
+      const mockMainFetchSpy = mockFetchs(fetchById, MOCK_FETCH_BY_ID_RETURN_DRINK);
+      const mockFetchByNameSpy = mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN_MEALS);
+
+      renderWithRouter(<Details />, { initialEntries: ['/drinks/17256'] });
+
+      expect(mockMainFetchSpy).toHaveBeenCalledTimes(1);
+      expect(mockMainFetchSpy).toHaveBeenCalledWith('', 'drinks');
+      expect(mockFetchByNameSpy).toHaveBeenCalledTimes(1);
+      expect(mockFetchByNameSpy).toHaveBeenCalledWith('', 'meals');
+
+      const { cards, titles } = await getRecomendations();
+
+      expect(cards).toHaveLength(6);
+
+      titles.forEach((title, index) => {
+        expect(title).toHaveTextContent(mealFormattedSearchByNameMock[index].name);
+      });
+    });
+  });
+
+  it('should render a error message if the fetch fails', async () => {
+    const mockMainFetchSpy = mockFetchs(fetchById, { recipe: undefined, loading: false, error: 'error' });
+    const mockFetchByNameSpy = mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN_DRINKS);
+
+    renderWithRouter(<Details />, INITIAL_ENTRIES);
+
+    expect(mockMainFetchSpy).toHaveBeenCalledTimes(1);
+    expect(mockFetchByNameSpy).toHaveBeenCalledTimes(1);
+
+    expect(await screen.findByText('Erro ao carregar')).toBeInTheDocument();
+  });
+
+  it('should render a loading message', async () => {
+    const mockMainFetchSpy = mockFetchs(fetchById, { recipe: undefined, loading: true, error: '' });
+    const mockFetchByNameSpy = mockFetchs(fetchByName, MOCK_FETCH_BY_NAME_RETURN_DRINKS);
+
+    renderWithRouter(<Details />, INITIAL_ENTRIES);
+
+    expect(mockMainFetchSpy).toHaveBeenCalledTimes(1);
+    expect(mockFetchByNameSpy).toHaveBeenCalledTimes(1);
+
+    expect(await screen.findByText('Loading...')).toBeInTheDocument();
   });
 });
--- src/tests/mock.ts
+++ src/tests/mock.ts
@@ -1,5 +1,4 @@
 import { DoneRecipeType } from '../@types/DoneRecipeType';
-import { MealRecipeType } from '../@types/MealRecipeType';
 
 const MEAL_THUMB = 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg';
 
@@ -53,92 +52,6 @@ export const mealMock = {
     strMeasure19: '',
     strMeasure20: '',
   }] };
-
-export const formattedMealMock: MealRecipeType = {
-  type: 'meals',
-  id: '52771',
-  category: 'Vegetarian',
-  name: 'Spicy Arrabiata Penne',
-  thumb: MEAL_THUMB,
-  area: 'Italian',
-  instructions: 'Cook the pasta in a pan',
-  video: 'https://www.youtube.com/watch?v=1IszT_guI08',
-  tags: null,
-  ingredients: [
-    'Penne Rigate',
-    'Olive Oil',
-    'Garlic',
-    'Red Chilli',
-    'Italian Tomatoes',
-    'Dried Oregano',
-    'Red Wine',
-    'Fresh Basil',
-    'Salt',
-    'Black Pepper',
-    'Parmesan',
-    'Water',
-  ],
-  measures: [
-    '1 pound',
-    '1/4 cup',
-    '1 clove',
-    '1',
-    '1 tin',
-    '1 teaspoon',
-    '1/2 cup',
-    '6 leaves',
-    'to taste',
-    'to taste',
-    'to taste',
-    '1/2 cup',
-  ],
-  dateModified: null,
-  creativeCommonsConfirmed: null,
-  imageSource: null,
-  drinkAlternate: null,
-  source: '',
-};
-
-export const mealFormattedSearchByNameMock: MealRecipeType[] = [
-  {
-    ...formattedMealMock,
-  },
-  {
-    ...formattedMealMock,
-    id: '52772',
-    name: 'Teriyaki Chicken',
-  },
-  {
-    ...formattedMealMock,
-    id: '52773',
-    name: 'Salad',
-  },
-  {
-    ...formattedMealMock,
-    id: '52774',
-    name: 'Pasta',
-  },
-  {
-    ...formattedMealMock,
-    id: '52775',
-    name: 'Pizza',
-  },
-  {
-    ...formattedMealMock,
-    id: '52776',
-    name: 'Sushi',
-  },
-  {
-    ...formattedMealMock,
-    id: '52777',
-    name: 'Burger',
-  },
-  {
-    ...formattedMealMock,
-    id: '52778',
-    name: 'Fries',
-  },
-];
 
 export const mealSearchedByIDMock = { meals:
     [

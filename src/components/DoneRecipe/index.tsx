b37911a8308d83360720764b74364b7a02792615
Paulo Afonso Cardoso Franco - Turma 36 <paulo_afonso2006@hotmail.com>
2024-03-01 14:58:59 -0300

    feat: adicionados botões de redirecionamento em imagem e nome da receita

--- src/components/DoneRecipe/index.tsx
+++ src/components/DoneRecipe/index.tsx
@@ -3,26 +3,36 @@ import { copyTextToClipBoard } from '../../utils/copyTextToClipBoard';
 
 function DoneRecipe(props: DoneRecipeProps) {
   const { doneRecipe, index } = props;
-
   const url = `${window.location.origin}/${doneRecipe.type}s/${doneRecipe.id}`;
   const copyText = async (text: string) => {
     const imgElement = document.getElementById('share-done-recipe-button');
     imgElement?.append(await copyTextToClipBoard(text));
   };
 
+  const handleClick = () => {
+    window.location.href = url;
+  };
   return (
     <div id={ `${index}-done-recipe-element` }>
-      <img
-        src={ doneRecipe.image }
-        data-testid={ `${index}-horizontal-image` }
-        alt={ doneRecipe.name }
-      />
+      <button onClick={ handleClick }>
+        <img
+          src={ doneRecipe.image }
+          data-testid={ `${index}-horizontal-image` }
+          alt={ doneRecipe.name }
+        />
+      </button>
+      <p>
+        <button onClick={ handleClick }>
+          <span data-testid={ `${index}-horizontal-name` }>
+            {doneRecipe.name}
+          </span>
+        </button>
+      </p>
       <p data-testid={ `${index}-horizontal-top-text` }>
         {doneRecipe.type === 'meal'
           ? `${doneRecipe.nationality} - ${doneRecipe.category}`
           : `${doneRecipe.alcoholicOrNot}`}
       </p>
-      <p data-testid={ `${index}-horizontal-name` }>{doneRecipe.name}</p>
       <p data-testid={ `${index}-horizontal-done-date` }>{doneRecipe.doneDate}</p>
       <button id={ `${index}-share-done-recipe-button` } onClick={ () => copyText(url) }>
         <img

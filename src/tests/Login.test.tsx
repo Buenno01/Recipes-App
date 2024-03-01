054163a028a27d8957c74a05b0e157b5b1386a12
Alisson Breno <alissonbreno36@hotmail.com>
2024-03-01 09:26:08 -0300

    feat: Iniciando requisito 7

--- src/tests/Login.test.tsx
+++ src/tests/Login.test.tsx
@@ -1,6 +1,5 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Login from '../pages/Login';
 import { renderWithRouter } from './utils';
 import App from '../App';
 

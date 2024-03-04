9cf2939f782603434f840ba8d4ecede16a9b1f21
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-04 11:17:31 -0300

    fix: alterado nome do componente conforme README

--- src/pages/Recipes/index.tsx
+++ src/pages/Recipes/index.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { RecipeOptionsType } from '../../@types/RecipeOptionsType';
 import useFetchDrinkOrFoodByName from '../../services/useFetchDrinkOrFoodByName';
+import useFetchCategories from '../../hooks/useFetchCategories';
 
 function Home() {
   const location = useLocation();
@@ -12,12 +13,27 @@ function Home() {
     recipes,
   } = useFetchDrinkOrFoodByName('', recipeType);
 
+  const {
+    error: errorCategories,
+    loading: loadingCategories,
+    categories,
+  } = useFetchCategories(recipeType);
+
   if (loading) return <p>Loading...</p>;
   if (error || !recipes) return <p>{error}</p>;
   return (
-    // Esse teste id é apenas para passar no teste de rotas do Login.test
-    <>
-      <div data-testid="divHome" />
+  // Esse teste id é apenas para passar no teste de rotas do Login.test
+    <div data-testid="divHome">
+      <ul>
+        {
+          !errorCategories
+          && categories.slice(0, 5).map((category) => (
+            <li data-testid={ `${category}-category-filter` } key={ category }>
+              {category}
+            </li>
+          ))
+}
+      </ul>
       {
         recipes.slice(0, 12).map((recipe, index) => (
           <div data-testid={ `${index}-recipe-card` } key={ recipe.name }>
@@ -29,8 +45,8 @@ function Home() {
             <p data-testid={ `${index}-card-name` }>{recipe.name}</p>
           </div>
         ))
-    }
-    </>
+      }
+    </div>
   );
 }
 

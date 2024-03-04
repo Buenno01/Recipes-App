6bd1d68aabba441bd2a06f38874aa10e6bc6867e
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-04 10:21:17 -0300

    feat: carregadas receitas na tela principal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { RecipeOptionsType } from '../../@types/RecipeOptionsType';
+import useFetchDrinkOrFoodByName from '../../services/useFetchDrinkOrFoodByName';
 
 function Home() {
+  const location = useLocation();
+  const recipeType: RecipeOptionsType = location.pathname.includes('meal')
+    ? 'meals' : 'drinks';
+  const {
+    error,
+    loading,
+    recipes,
+  } = useFetchDrinkOrFoodByName('', recipeType);
+
+  if (loading) return <p>Loading...</p>;
+  if (error || !recipes) return <p>{error}</p>;
   return (
     // Esse teste id é apenas para passar no teste de rotas do Login.test
-    <div data-testid="divHome">Home</div>
+    <>
+      <div data-testid="divHome" />
+      {
+        recipes.slice(0, 12).map((recipe, index) => (
+          <div data-testid={ `${index}-recipe-card` } key={ recipe.name }>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ recipe.thumb }
+              alt={ recipe.name }
+            />
+            <p data-testid={ `${index}-card-name` }>{recipe.name}</p>
+          </div>
+        ))
+    }
+    </>
   );
 }
 

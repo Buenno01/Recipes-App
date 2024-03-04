5a3034dc9a8bca090f61b0465d1ffccaf260fc3a
Paulo Afonso Cardoso Franco - Turma 36 <paulo_afonso2006@hotmail.com>
2024-03-03 23:26:40 -0300

    test: mock para favorite recipes (1 meal e 1 drink)

---
+++ src/tests/favoriRecipesMock.ts
@@ -0,0 +1,19 @@
+export const FAVORITE_RECIPE_MOCK = [
+  {
+    id: '17256',
+    type: 'drink',
+    nationality: '',
+    category: 'Cocktail',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Martinez 2',
+    image: 'https://www.thecocktaildb.com/images/media/drink/fs6kiq1513708455.jpg',
+  },
+  {
+    id: '52771',
+    type: 'meal',
+    nationality: 'Italian',
+    category: 'Vegetarian',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Spicy Arrabiata Penne',
+    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  }];

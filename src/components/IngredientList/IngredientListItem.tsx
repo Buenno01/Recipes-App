ccc9b6fb47beef31f4270c53e69ffe3f01e86f3e
dennys-terra <dennys.jornadadev@gmail.com>
2024-03-06 19:24:34 -0300

    feat: ingredientes finalizados são riscados da lista e salvos no local storage

--- src/components/IngredientList/IngredientListItem.tsx
+++ src/components/IngredientList/IngredientListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 
@@ -12,15 +12,40 @@ function IngredientListItem({ ingredient, measure, index }: IngredientListItemPr
   const { id } = useParams();
   const location = useLocation();
   const recipeType = location.pathname.includes('meals') ? 'meals' : 'drinks';
-  const inProgress = useLocalStorage('inProgressRecipes', {});
+  const [inProgress, setInProgress] = useLocalStorage(
+    'inProgressRecipes',
+    { drinks: {}, meals: {} },
+  );
+  const thisProgress = inProgress[recipeType][id]
+    ? inProgress[recipeType][id] : [];
+  const INITIAL_STATE = !!thisProgress.includes(ingredient);
+  const [isChecked, setIsChecked] = useState(INITIAL_STATE);
+
+  const handleChange = () => {
+    if (!isChecked) {
+      setInProgress({ ...inProgress,
+        [recipeType]: { ...inProgress[recipeType],
+          [id]: [...thisProgress, ingredient] } });
+    } else {
+      setInProgress({ ...inProgress,
+        [recipeType]: { ...inProgress[recipeType],
+          [id]: thisProgress.filter((i : string) => i !== ingredient) } });
+    }
+    setIsChecked(!isChecked);
+  };
   return (
     <li>
       {location.pathname.includes('in-progress') ? (
-        <label data-testid={ `${index}-ingredient-step` } className="flex gap-2">
+        <label
+          data-testid={ `${index}-ingredient-step` }
+          className={ `flex gap-2 ${isChecked && 'line-through'}` }
+        >
           <input
             type="checkbox"
             name={ `ingredient-${ingredient}` }
             value={ ingredient }
+            checked={ isChecked }
+            onChange={ handleChange }
           />
           {measure ? `${measure} - ` : ''}
           {ingredient}

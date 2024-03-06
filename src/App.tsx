b4f12a360c91ab9c3329c30431f5c1a0d33222a0
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-06 13:09:05 -0300

    fix: consertado nome do componente RecipeInProgress

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
 import RecipeDetails from './pages/RecipeDetails';
-import InProgress from './pages/InProgress';
+import RecipeInProgress from './pages/RecipeInProgress';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
@@ -18,8 +18,8 @@ function App() {
         <Route path="/meals/:id" element={ <RecipeDetails /> } />
         <Route path="/drinks/:id" element={ <RecipeDetails /> } />
         <Route path="/favorite-recipes" element={ <FavoriteRecipes /> } />
-        <Route path="/meals/:id/in-progress" element={ <InProgress /> } />
-        <Route path="/drinks/:id/in-progress" element={ <InProgress /> } />
+        <Route path="/meals/:id/in-progress" element={ <RecipeInProgress /> } />
+        <Route path="/drinks/:id/in-progress" element={ <RecipeInProgress /> } />
         <Route path="/profile" element={ <Profile /> } />
         <Route path="/done-recipes" element={ <DoneRecipes /> } />
         <Route path="*" element={ <p> Nao achei </p> } />

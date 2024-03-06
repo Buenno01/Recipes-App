c5af030ab417dfd7dbf5d6c0dfd2613b52f77715
dennys-terra <dennys.jornadadev@gmail.com>
2024-03-06 18:43:14 -0300

    feat: renderiza condicionalmente os ingredientes

--- src/components/IngredientList/IngredientListItem.tsx
+++ src/components/IngredientList/IngredientListItem.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 
 type IngredientListItemProps = {
   ingredient: string;
@@ -7,15 +9,29 @@ type IngredientListItemProps = {
 };
 
 function IngredientListItem({ ingredient, measure, index }: IngredientListItemProps) {
+  const { id } = useParams();
+  const location = useLocation();
+  const recipeType = location.pathname.includes('meals') ? 'meals' : 'drinks';
+  const inProgress = useLocalStorage('inProgressRecipes', {});
   return (
-    <li
-      data-testid={ `${index}-ingredient-name-and-measure` }
-    >
-      <label className="flex gap-2">
-        <input type="checkbox" name={ `ingredient-${ingredient}` } value={ ingredient } />
-        {measure ? `${measure} - ` : ''}
-        {ingredient}
-      </label>
+    <li>
+      {location.pathname.includes('in-progress') ? (
+        <label data-testid={ `${index}-ingredient-step` } className="flex gap-2">
+          <input
+            type="checkbox"
+            name={ `ingredient-${ingredient}` }
+            value={ ingredient }
+          />
+          {measure ? `${measure} - ` : ''}
+          {ingredient}
+        </label>
+      ) : (
+        <p data-testid={ `${index}-ingredient-name-and-measure` }>
+          {measure ? `${measure} - ` : ''}
+          {ingredient}
+        </p>
+      ) }
+
     </li>
   );
 }

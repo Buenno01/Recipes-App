baa666abeee75f2a5a35133b7f149c54360edb6f
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-07 11:20:22 -0300

    fix: consertados endereços de imagem caso a categoria seja all dependendo da pagina ser drinks ou meals

--- src/components/atoms/CategoryBtn.tsx
+++ src/components/atoms/CategoryBtn.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 interface CategoryBtnProps extends React.HTMLAttributes<HTMLButtonElement> {
   category: string;
@@ -6,15 +7,19 @@ interface CategoryBtnProps extends React.HTMLAttributes<HTMLButtonElement> {
 
 function CategoryBtn({ category, ...rest }: CategoryBtnProps) {
   const formattedCategory = category.toLowerCase().replaceAll(/[^a-z0-9]+/g, '-');
+  const { pathname } = useLocation();
+  const recipeType = pathname.includes('meals') ? 'meals' : 'drinks';
+  const imageAddress = formattedCategory !== 'all' ? formattedCategory : recipeType;
   return (
     <button
       { ...rest }
       data-testid={ `${category}-category-filter` }
+      className="h-12 w-12 rounded-full border flex items-center justify-center"
     >
-      <img src={ `src/assets/images/category-${formattedCategory}-icon.svg` } alt="" />
-      <span>
-        {category}
-      </span>
+      <img
+        src={ `src/assets/images/category-${imageAddress}-icon.svg` }
+        alt=""
+      />
     </button>
   );
 }

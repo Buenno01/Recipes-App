903476660a3d4e0df2391cbc588c88dd903354f4
Paulo Afonso Cardoso Franco - Turma 36 <paulo_afonso2006@hotmail.com>
2024-03-07 00:00:35 -0300

    style: Done Recipe card individual

--- src/components/DoneRecipe/index.tsx
+++ src/components/DoneRecipe/index.tsx
@@ -14,24 +14,26 @@ function DoneRecipe(props: DoneRecipeProps) {
   };
   return (
     <div className="flex h-64 mb-1">
-      <div className="w-1/2 h-full">
+      <div className="w-1/2 h-full flex-shrink-0">
         <ClickableImage
           recipe={ doneRecipe }
           onClick={ handleClick }
           index={ index }
         />
       </div>
-      <div className="w-1/2 h-full text-center mt-4">
-        <button onClick={ handleClick }>
-          <p data-testid={ `${index}-horizontal-name` } className="font-bold">
-            {doneRecipe.name}
-          </p>
-        </button>
-        <CategoryDrinkOrMeal
-          recipe={ doneRecipe }
-          index={ index }
-          className="text-sm text-gray-500"
-        />
+      <div className="w-full h-full mt-4 text-left pl-4">
+        <div className="">
+          <button onClick={ handleClick }>
+            <p data-testid={ `${index}-horizontal-name` } className="font-bold text-left">
+              {doneRecipe.name}
+            </p>
+          </button>
+          <CategoryDrinkOrMeal
+            recipe={ doneRecipe }
+            index={ index }
+            className="text-sm text-gray-500 text-left"
+          />
+        </div>
         <div className="mt-10">
           <span>Done in: </span>
           <span
@@ -40,18 +42,24 @@ function DoneRecipe(props: DoneRecipeProps) {
             {formatDate(doneRecipe.doneDate)}
           </span>
         </div>
-        {
+        <div className="flex">
+          {
       doneRecipe.tags?.map((tagName: string, tagIndex: number) => {
         if (tagIndex >= 2) return;
         return (
-          <p key={ tagName } data-testid={ `${index}-${tagName}-horizontal-tag` }>
+          <p
+            key={ tagName }
+            data-testid={ `${index}-${tagName}-horizontal-tag` }
+            className="px-2 ml-2 mt-16 bg-gray-200 rounded-full"
+          >
             {tagName}
           </p>
         );
       })
 }
+        </div>
       </div>
-      <div className="w-auto h-full bg-blue-500">
+      <div className="w-1/6 h-full mt-4">
         <ShareButton
           alt="Compatilhar"
           dataTestID={ `${index}-horizontal-share-btn` }

dfaa4f556762cf73bced6052e4d9a4a6a15f757a
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-08 09:23:12 -0300

    estilizada pagina de perfil

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 
 interface LinkButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
-  text: string
+  children?: React.ReactNode
+  text?: string
   to: string
 }
 
-function LinkButton({ text, to, onClick, ...rest }: LinkButtonProps) {
+function LinkButton({ children = '', text = '', to, onClick, ...rest }: LinkButtonProps) {
   const navigate = useNavigate();
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -15,6 +16,7 @@ function LinkButton({ text, to, onClick, ...rest }: LinkButtonProps) {
 
   return (
     <button { ...rest } onClick={ (e) => handleClick(e) }>
+      { children }
       { text }
     </button>
   );
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,37 +1,67 @@
 import React from 'react';
 import LinkButton from '../../components/LinkButton';
+import logoutBtn from '../../assets/images/logoutBtn.svg';
+import favoriteRecipesBtn from '../../assets/images/favoriteRecipesBtn.svg';
+import doneRecipesBtn from '../../assets/images/doneRecipesBtn.svg';
 
 function Profile() {
   const user = JSON.parse(localStorage.getItem('user') || '{"email": ""}');
-  const clearUser = () => localStorage.clear();
+  const clearStorage = () => localStorage.clear();
 
   const buttons = [
-    { text: 'Done Recipes', to: '/done-recipes', testId: 'profile-done-btn' },
-    { text: 'Favorite Recipes', to: '/favorite-recipes', testId: 'profile-favorite-btn' },
-    { text: 'Logout', to: '/', testId: 'profile-logout-btn' },
+    {
+      text: 'Done Recipes',
+      to: '/done-recipes',
+      'data-testid': 'profile-done-btn',
+      imgSrc: doneRecipesBtn,
+    },
+    {
+      text:
+      'Favorite Recipes',
+      to: '/favorite-recipes',
+      className: 'border-b-2 border-t-2',
+      'data-testid': 'profile-favorite-btn',
+      imgSrc: favoriteRecipesBtn,
+    },
+    {
+      text: 'Logout',
+      to: '/',
+      onClick: clearStorage,
+      'data-testid': 'profile-logout-btn',
+      imgSrc: logoutBtn,
+    },
   ];
 
   return (
-    <div data-testid="divProfilePage">
+    <>
       Perfil
-      <p data-testid="profile-email">
+      <p
+        className="text-lg text-center text-primary-black font-bold"
+        data-testid="profile-email"
+      >
         { user.email }
       </p>
-      <ul>
+      <ul
+        className="text-lg text-primary-gray w-10/12 mx-auto"
+      >
         {
-          buttons.map(({ text, to, testId }) => (
-            <li key={ text }>
+          buttons.map(({ imgSrc, className, ...att }) => (
+            <li
+              className={ `px-8 py-8
+              ${className || ''}` }
+              key={ att.text }
+            >
               <LinkButton
-                data-testid={ testId }
-                text={ text }
-                to={ to }
-                onClick={ text === 'Logout' ? clearUser : undefined }
-              />
+                className="flex items-center gap-4"
+                { ...att }
+              >
+                <img src={ imgSrc } alt={ `${att.text}-button` } />
+              </LinkButton>
             </li>
           ))
         }
       </ul>
-    </div>
+    </>
   );
 }
 

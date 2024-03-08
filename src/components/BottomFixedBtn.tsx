6fbc3070adbfaa17e0844277194bf2054003042b
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-08 11:27:45 -0300

    fix: o botao rejeitava a mudança de disabled pelas propriedades extendidas

--- src/components/BottomFixedBtn.tsx
+++ src/components/BottomFixedBtn.tsx
@@ -1,13 +1,15 @@
-import { HTMLAttributes, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
-interface BottomFixedBtnProps extends HTMLAttributes<HTMLButtonElement> {
+interface BottomFixedBtnProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: ReactNode
+  disabled?: boolean
 }
 
-function BottomFixedBtn({ children, ...props }: BottomFixedBtnProps) {
+function BottomFixedBtn({ disabled = false, children, ...props }: BottomFixedBtnProps) {
   return (
     <button
       { ...props }
+      disabled={ disabled }
       className="fixed bottom-0 h-14 rounded-lg w-11/12 bg-primary-yellow
       text-white font-bold left-1/2 right-1/2 transform -translate-x-1/2
       uppercase text-lg mb-2"

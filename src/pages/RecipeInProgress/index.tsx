4cf9027b9c65fbac13ff7012d2d0063f94d9d199
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-08 11:20:23 -0300

    feat: o botao agora fica desabilitado até que todos os ingredientes estejam marcados

--- src/pages/RecipeInProgress/index.tsx
+++ src/pages/RecipeInProgress/index.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
 import { useLocation, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Video from '../../components/Video';
 import Title from '../../components/Title';
 import { RecipeOptionsType } from '../../@types/RecipeOptionsType';
 import useFetch from '../../hooks/useFetch';
 import IngredientsCheckBox from '../../components/IngredientsCheckBox';
+import Instructions from '../../components/Instructions';
+import BottomFixedBtn from '../../components/BottomFixedBtn';
+import { useInProgressContext } from '../../contexts/InProgressContext';
 
 function RecipeInProgress() {
   const { id } = useParams();
   const location = useLocation();
   const recipeType : RecipeOptionsType = location.pathname
     .includes('meals') ? 'meals' : 'drinks';
+  const [disabled, setDisabled] = useState(true);
 
   const { data } = useFetch<'id'>(id || '', recipeType, 'id');
+
+  const {
+    finishRecipe,
+    progress,
+  } = useInProgressContext();
+
+  const handleDisabled = (recipeProgress: string[], totalIngredients: string[]) => {
+    if (recipeProgress.length === totalIngredients.length) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  useEffect(() => {
+    if (id && data && data.length > 0) {
+      handleDisabled(progress[recipeType][id], data[0].ingredients);
+    }
+  }, [data, id, progress, recipeType]);
 
   if (!data) return;
   return (
     <>
       <Title recipe={ data[0] } />
-      <IngredientsCheckBox recipe={ data[0] } />
-      <p data-testid="instructions">{data[0].instructions}</p>
+      <IngredientsCheckBox
+        recipe={ data[0] }
+      />
+      <Instructions instructions={ data[0].instructions } />
       <Video recipe={ data[0] } />
-      <button data-testid="finish-recipe-btn">finish recipe</button>
+      <BottomFixedBtn
+        data-testid="finish-recipe-btn"
+        disabled={ disabled }
+        onClick={ () => finishRecipe(recipeType, id || '') }
+      >
+        Finish Recipe
+      </BottomFixedBtn>
     </>
   );
 }

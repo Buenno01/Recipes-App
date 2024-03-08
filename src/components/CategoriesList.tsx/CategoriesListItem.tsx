2fd054da970bb7e78bc3e82ad90bfe4f802b82a0
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-08 09:28:22 -0300

    style: consertada estilização de categorias com mais de uma palavra

--- src/components/CategoriesList.tsx/CategoriesListItem.tsx
+++ src/components/CategoriesList.tsx/CategoriesListItem.tsx
@@ -8,13 +8,13 @@ type CategoriesListItemProps = {
 
 function CategoriesListItem({ category, handleClick }: CategoriesListItemProps) {
   return (
-    <li className="flex flex-col text-center gap-1" key={ category }>
+    <li className="flex flex-col max-w-11 min-h-20 text-center gap-1" key={ category }>
       <CategoryBtn
         onClick={ handleClick }
         data-testid={ `${category}-category-filter` }
         category={ category }
       />
-      <span className="text-xxs font-light text-gray-500">
+      <span className="text-xxs font-light text-gray-500 whitespace-normal">
         {category}
       </span>
     </li>

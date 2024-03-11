ecac660b11f5c83c65b8c767cd9aaa110148e9fe
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-11 10:35:29 -0300

    fix: consertado bug que algumas vezes o header e o footer apareciam na pagina errada

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -2,71 +2,85 @@ import React, { useEffect, useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import getPageTitle from '../utils/getPageTitle';
+
+// Rota "/": não tem header.
+// Rota "/meals": tem o header com o título "Meals" e os ícones de perfil e pesquisa.
+// Rota "/drinks": tem o header com o título "Drinks" e os ícones de perfil e pesquisa.
+// Rota "/meals/:id": não tem header.
+// Rota "/drinks/:id": não tem header.
+// Rota "/meals/:id/in-progress": não tem header.
+// Rota "/drinks/:id/in-progress": não tem header.
+// Rota "/profile": tem o header com o título "Profile" e o ícone de perfil, mas sem o ícone de pesquisa.
+// Rota "/done-recipes": tem o header com o título "Done Recipes" e o ícone de perfil, mas sem o ícone de pesquisa.
+// Rota "/favorite-recipes": tem o header com o título "Favorite Recipes" e o ícone de perfil, mas sem o ícone de pesquisa.
+
+// Rota "/": não deve ter footer.
+// Rota "/meals": deve ter footer.
+// Rota "/drinks": deve ter footer.
+// Rota "/meals/:id": não deve ter footer.
+// Rota "/drinks/:id": não deve ter footer.
+// Rota "/meals/:id/in-progress": não deve ter footer.
+// Rota "/drinks/:id/in-progress": não deve ter footer.
+// Rota "/profile": deve ter footer.
+// Rota "/done-recipes": não deve ter footer.
+// Rota "/favorite-recipes": não deve ter footer.
 
 function Layout() {
-  const location = useLocation();
+  const { pathname } = useLocation();
   const [isHeaderVisible, setIsHeaderVisible] = useState(false);
   const [isFooterVisible, setIsFooterVisible] = useState(false);
   const [titlePage, setTitlePage] = useState('');
-  const [profileIcon, setProfileIcon] = useState(false);
-  const [searchIcon, setSeachIcon] = useState(false);
+  const [searchIcon, setSearchIcon] = useState(false);
+
+  const DETAILS_MEALS = /^\/meals\/\d+\/?$/;
+  const DETAILS_DRINKS = /^\/drinks\/\d+\/?$/;
+  const IN_PROGRESS = /in-progress/i;
+  const PROFILE = /profile/i;
+  const DONE_RECIPES = /done-recipes/i;
+  const FAVORITE_RECIPES = /favorite-recipes/i;
+  const MEALS = /^\/meals\/?$/;
+  const DRINKS = /^\/drinks\/?$/;
+  const ROOT = /^\/$/;
 
   useEffect(() => {
-    switch (location.pathname) {
-      case '/':
+    switch (true) {
+      case DETAILS_MEALS.test(pathname):
+      case DETAILS_DRINKS.test(pathname):
+      case IN_PROGRESS.test(pathname):
+      case ROOT.test(pathname):
         setIsHeaderVisible(false);
         setIsFooterVisible(false);
         break;
-      case '/meals':
-        setIsHeaderVisible(true); setSeachIcon(true); setProfileIcon(true);
-        setTitlePage('Meals');
-        setIsFooterVisible(true);
-        break;
-      case '/drinks':
-      case '/drinks/':
-        setIsHeaderVisible(true); setSeachIcon(true); setProfileIcon(true);
-        setTitlePage('Drinks');
-        setIsFooterVisible(true);
-        break;
-      case '/meals/:id':
-        setIsHeaderVisible(false);
-        break;
-      case '/drinks/:id':
-        setIsHeaderVisible(false);
-        break;
-      case '/meals/:id/in-progress':
-        setIsHeaderVisible(false);
-        break;
-      case '/drinks/:id/in-progress':
-        setIsHeaderVisible(false);
-        break;
-      case '/profile':
+      case PROFILE.test(pathname):
         setIsHeaderVisible(true);
-        setProfileIcon(true);
-        setTitlePage('Profile');
         setIsFooterVisible(true);
+        setSearchIcon(false);
         break;
-      case '/done-recipes':
+      case DONE_RECIPES.test(pathname):
+      case FAVORITE_RECIPES.test(pathname):
         setIsHeaderVisible(true);
-        setProfileIcon(true);
-        setTitlePage('Done Recipes');
+        setIsFooterVisible(false);
+        setSearchIcon(false);
         break;
-      case '/favorite-recipes':
+      case MEALS.test(pathname):
+      case DRINKS.test(pathname):
         setIsHeaderVisible(true);
-        setProfileIcon(true);
-        setTitlePage('Favorite Recipes');
+        setIsFooterVisible(true);
+        setSearchIcon(true);
         break;
       default:
         break;
     }
-  }, [location.pathname]);
+    setTitlePage(getPageTitle(pathname));
+  }, [pathname]);
 
   return (
     <>
       {isHeaderVisible && (
         <Header
           titlePage={ titlePage }
-          profileIcon={ profileIcon }
+          profileIcon
           searchIcon={ searchIcon }
         />
       )}
